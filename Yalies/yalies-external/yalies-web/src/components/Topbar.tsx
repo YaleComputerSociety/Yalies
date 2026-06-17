7bfe1bbd95b095a909168da3a57ea884d3b5282e
Jeet Parikh <jeetparikh06@gmail.com>
2026-06-17 02:15:12 -0700

    Remove maintenance banner

--- Yalies/yalies-external/yalies-web/src/components/Topbar.tsx
+++ Yalies/yalies-external/yalies-web/src/components/Topbar.tsx
@@ -2,13 +2,8 @@ import styles from "./topbar.module.scss";
 
 export default function Topbar({ children }: { children: React.ReactNode }) {
 	return (
-		<>
-			<div className={styles.announcement}>
-				🚧 Yalies is currently down for summer maintenance by Y/CS. Expect things to be broken.
-			</div>
-			<div id={styles.topbar}>
-				{children}
-			</div>
-		</>
+		<div id={styles.topbar}>
+			{children}
+		</div>
 	);
 };
